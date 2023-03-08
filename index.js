2988c28470a5aca86921b39dfa202316a23c6f9f
CristhianCaje <cristhian77784@gmail.com>
2023-03-08 02:48:08 -0300

    Actualizacion js

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-let enviarAlerta = enviarAlerta => {
+/*let enviarAlerta = enviarAlerta => {
     swal("","El mensaje fue enviado","success");
+}*/
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+const expresiones = {
+    nombre:/^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    email:/^[a-zA-Z-9_.+-]+@[a-zA-Z0-9-]+\[a-zA-Z0-9-.]+$/
 }
+const validarFormulario = (e) => {
+    switch(e.target.name){
+        case "nombre":
+            if(expresiones.nombre.test(e.target.value)){
+                document.getElementById('nom').classList.remove('incorrecto');
+                document.getElementById('nom').classList.add('correcto');
+            }else{
+                document.getElementById('nom').classList.add('incorrecto');
+            }
+        break;  
+        case"email":
+        break;
+        case"celular":
+        break;
+        case"descripcion":
+        break;
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+/*formulario.addEventListener('submit',(e) =>{
+    e.preventDefault();
+})*/
