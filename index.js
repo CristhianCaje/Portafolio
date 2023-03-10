dca7a9be11c7e2821f589aa343434100f7860fa7
CristhianCaje <cristhian77784@gmail.com>
2023-03-10 00:09:51 -0300

    Empezamos con JS

---
+++ index.js
@@ -0,0 +1,4 @@
+const gnb = document.getElementById('trabajo')
+document.getElementById('trabajo').addEventListener('click', function() { 
+    document.write('<div><h1>Hola que tal?</h1></div>')
+   })
