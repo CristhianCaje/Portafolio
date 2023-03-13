26acddb32650ed44d442fe80f4fc2d4946f5736f
CristhianCaje <cristhian77784@gmail.com>
2023-03-13 00:20:30 -0300

    Agregando las validaciones de Formulario con JS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Imagen de perfil completo*/
 const fulImgBox = document.getElementById('fulImgBox');
 const fulImg = document.getElementById('fulImg');
 
@@ -7,3 +8,28 @@ openFulImg = () => {
 closeImg = () =>{
     fulImgBox.style.display = "none";
 }
+
+/*validaciones de furmulario*/
+const datos = {
+    nombre: document.getElementById('nombre'),
+    email: document.getElementById('email'),
+    telefono: document.getElementById('telefono'),
+    mensaje: document.getElementById('mensaje')
+}
+const validarCampos = (menssage, e) =>{
+        const campo = e.target.value;
+        const inp = e.target;
+        if(campo.trim().length ===0 ){
+            inp.style.border = 'red solid 1px';
+            inp.nextElementSibling.classList.add('fail');
+            inp.nextElementSibling.innerText = menssage;
+        }else{
+            inp.style.border = 'green solid 1px';
+            inp.nextElementSibling.classList.remove('fail')
+            inp.nextElementSibling.innerText = '';
+        }
+}
+datos.nombre.addEventListener('blur', (e) => validarCampos("Favor completar su Nombre y Apellido",e));
+datos.email.addEventListener('blur', (e) => validarCampos("Favor completar su Correo electronico",e));
+datos.telefono.addEventListener('blur', (e) => validarCampos("Favor completar su Numero de Telefono",e));
+datos.mensaje.addEventListener('blur', (e) => validarCampos("Favor agragar el mensaje que quiere enviar",e));
