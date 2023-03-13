fdf7b2116312701369caa7111dd790647aa31d06
CristhianCaje <cristhian77784@gmail.com>
2023-03-12 21:16:38 -0300

    Terminado el visor de imagen para el Perfil

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const gnb = document.getElementById('trabajo')
-document.getElementById('trabajo').addEventListener('click', function() { 
-    document.write('<div><h1>Hola que tal?</h1></div>')
-   })
+const fulImgBox = document.getElementById('fulImgBox');
+const fulImg = document.getElementById('fulImg');
+
+openFulImg = () => {
+    fulImgBox.style.display = 'flex';
+}
+closeImg = () =>{
+    fulImgBox.style.display = "none";
+}
